3382ea679696c2e4b76d00fd48d8ed4b45be8601
fareha1069 <138511916+fareha1069@users.noreply.github.com>
2023-09-13 03:31:14 +0500

    Add files via upload

---
+++ module4/script.js
@@ -0,0 +1,12 @@
+(function () {
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+  for (i in names) {
+    var firstLetter = names[i].charAt(0);
+    firstLetter = firstLetter.toLocaleLowerCase();
+    if (firstLetter == "j") 
+      byeSpeaker.speak(names[i]);
+    else 
+      helloSpeaker.speak(names[i]);
+  }
+})();
